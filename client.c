cd57d51f1f2a04f5d18c126ff2c4a890a50e990e
V <1477656117@qq.com>
2023-05-04 00:14:47 +0800

    23.5.3

--- client.c
+++ client.c
@@ -11,8 +11,8 @@
 short apple[2];
 char map[ROW * COL], map_old[ROW * COL];
 
-void cursor_hide() {
-    CONSOLE_CURSOR_INFO curInfo = {.dwSize = 1, .bVisible = FALSE};
+void cursor_show(int flag) {
+    CONSOLE_CURSOR_INFO curInfo = {.dwSize = 1, .bVisible = flag};
     SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &curInfo);
 }
 
@@ -33,21 +33,25 @@ void render_map() {
     memcpy(map_old, map, sizeof(map));
 }
 
-int init_ui() {
-    system("cls");
-    int ret;
-    printf("choose a game mode(control:\"wasd\",quit in game:\"q\"):\n");
-    printf("1.singleplayer\n");
-    printf("2.multiplayer\n");
-    printf("3.quit\n");
-    scanf("%d", &ret);
-    return ret;
+void print_info(int flag) {
+    cursor_go(COL + 1, 0);
+    printf("control: \"wasd\"");
+    cursor_go(COL + 1, 1);
+    printf("quit: \"q\"");
+    if (flag) {
+        cursor_go(COL + 1, 3);
+        printf("player1 score: ");
+        cursor_go(COL + 1, 4);
+        printf("player2 score: ");
+    } else {
+        cursor_go(COL + 1, 3);
+        printf("player score: ");
+    }
 }
 
 void singleplayer() {
     system("cls");
 
-    cursor_hide();
     init_map(map);
     init_apple(map, apple);
 
@@ -57,6 +61,7 @@ void singleplayer() {
     char input;
     memset(map_old, AIR, sizeof(map_old));
     render_map();
+    print_info(0);
     while (1) {
         input = DEFAULT_INPUT;
         if (_kbhit()) {
@@ -65,6 +70,8 @@ void singleplayer() {
         process_input(&player, input, &direction);
         if (direction == QUIT_DIRECTION) {
             //player quit
+            cursor_go(0, ROW);
+            system("pause");
             break;
         }
         if (player.current_direction != INIT_DIRECTION) {
@@ -78,11 +85,16 @@ void singleplayer() {
             break;
         }
         render_map();
+        cursor_go(COL + 15, 3);
+        printf("%d", player.len - 1);
         Sleep(TIME_WAIT);
     }
 }
 
 void multiplayer() {
+    system("cls");
+    cursor_show(1);
+
     //init winsock
     WSADATA wsaData;
     WSAStartup(MAKEWORD(2, 2), &wsaData);
@@ -109,16 +121,18 @@ void multiplayer() {
     int iResult = connect(ConnectSocket, (SOCKADDR *) &hints, sizeof(hints));
     if (iResult == SOCKET_ERROR) {
         closesocket(ConnectSocket);
-        printf("failed to connect to the server.");
+        printf("failed to connect to the server.\n");
         system("pause");
         return;
     }
 
     //connected
     system("cls");
-    cursor_hide();
+    cursor_show(0);
+
     char senddata;
     memset(map_old, AIR, sizeof(map_old));
+    print_info(1);
     while (1) {
         //receive data from server
         recv(ConnectSocket, map, ROW * COL, 0);
@@ -130,6 +144,25 @@ void multiplayer() {
             break;
         }
         render_map();
+        int score[2] = {0};
+        for (int i = 0; i < ROW; i++) {
+            for (int j = 0; j < COL; j++) {
+                if (map[i * COL + j] == SNAKE_1) {
+                    score[0]++;
+                } else if (map[i * COL + j] == SNAKE_2) {
+                    score[1]++;
+                }
+            }
+        }
+        cursor_go(COL + 16, 3);
+        printf("%d", score[0] - 1);
+        if (score[1]) {
+            cursor_go(COL + 16, 4);
+            printf("%d", score[1] - 1);
+        } else {
+            cursor_go(COL + 16, 4);
+            printf("unconnected");
+        }
 
         //keep sending data to server
         senddata = DEFAULT_INPUT;
@@ -144,18 +177,60 @@ void multiplayer() {
     WSACleanup();
 }
 
+int init_ui() {
+    system("cls");
+
+    cursor_show(0);
+    printf("choose a game mode:\n");
+    printf("   singleplayer\n");
+    printf("   multiplayer\n");
+    printf("   quit\n");
+    char input;
+    short loc = 1;
+    while (1) {
+        cursor_go(0, loc);
+        printf("->");
+        input = DEFAULT_INPUT;
+        if (_kbhit()) {
+            input = (char) getch();
+        }
+        if (input == 'w') {
+            cursor_go(0, loc);
+            printf("  ");
+            if (loc == 1) {
+                loc = 3;
+            } else {
+                loc--;
+            }
+        } else if (input == 's') {
+            cursor_go(0, loc);
+            printf("  ");
+            if (loc == 3) {
+                loc = 1;
+            } else {
+                loc++;
+            }
+        } else if (input == 13) {
+            switch (loc) {
+                case 1:
+                    singleplayer();
+                    return 1;
+                case 2:
+                    multiplayer();
+                    return 1;
+                case 3:
+                    return 0;
+                default:
+                    break;
+            }
+        }
+    }
+}
+
 int main() {
     srand((unsigned) time(NULL));
 
-    while (1) {
-        int input = init_ui();
-        if (input == 1) {
-            singleplayer();
-        } else if (input == 2) {
-            multiplayer();
-        } else if (input == 3) {
-            break;
-        }
+    while (init_ui()) {
     }
     return 0;
 }
